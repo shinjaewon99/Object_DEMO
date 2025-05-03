8207b6ed65a2d3fa27a6102cb29ff72e9a602e19
shinjaewon99 <tlswodnjs99@naver.com>
2025-05-03 20:05:58 +0900

    feat : 서비스 레이어 추가

---
+++ src/main/java/_01_ticet/Theater.java
@@ -0,0 +1,26 @@
+package _01_ticet;
+
+public class Theater {
+    private TicketSeller ticketSeller;
+
+    /**
+     * 입장시 TicketSeller를 거쳐야 함
+     *
+     * @param ticketSeller
+     */
+    public Theater(TicketSeller ticketSeller) {
+        this.ticketSeller = ticketSeller;
+    }
+
+    public void enter(Audience audience) {
+        if (audience.getBag().hasInvitation()) {
+            Ticket ticket = ticketSeller.getTicketOffice().getTicket();
+            audience.getBag().setTicket(ticket);
+        } else {
+            Ticket ticket = ticketSeller.getTicketOffice().getTicket();
+            audience.getBag().minusAmount(ticket.getFee());
+            ticketSeller.getTicketOffice().plusAmount(ticket.getFee());
+            audience.getBag().setTicket(ticket);
+        }
+    }
+}
---
+++ src/main/java/_01_ticet/TicketSeller.java
@@ -0,0 +1,13 @@
+package _01_ticet;
+
+public class TicketSeller {
+    private TicketOffice ticketOffice;
+
+    public TicketSeller(TicketOffice ticketOffice) {
+        this.ticketOffice = ticketOffice;
+    }
+
+    public TicketOffice getTicketOffice() {
+        return ticketOffice;
+    }
+}
