3c13e0b60b6a1be63997adaabe7f3b81ecaeceea
shinjaewon99 <tlswodnjs99@naver.com>
2025-05-05 18:46:50 +0900

    refactor: 객체 캡슐화 작업

--- src/main/java/_01_ticet/Bag.java
+++ src/main/java/_01_ticet/Bag.java
@@ -5,6 +5,16 @@ public class Bag {
     private Invitation invitation;
     private Ticket ticket;
 
+    public Long hold(Ticket ticket) {
+        if (hasInvitation()) {
+            setTicket(ticket);
+            return 0L;
+        } else {
+            minusAmount(ticket.getFee());
+            return ticket.getFee();
+        }
+    }
+
     public Bag(long amount) {
         this(null, amount);
     }
@@ -14,23 +24,19 @@ public class Bag {
         this.amount = amount;
     }
 
-    public boolean hasInvitation() {
+    private boolean hasInvitation() {
         return invitation != null;
     }
 
-    public boolean hasTicket() {
+    private boolean hasTicket() {
         return ticket != null;
     }
 
-    public void setTicket(Ticket ticket) {
+    private void setTicket(Ticket ticket) {
         this.ticket = ticket;
     }
 
-    public void minusAmount(Long amount) {
+    private void minusAmount(Long amount) {
         this.amount = amount;
-    }
-
-    public void plusAmount(Long amount) {
-        this.amount += amount;
     }
 }
