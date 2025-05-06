10b4e63741489f4197cdfa966b33b632e2eea588
shinjaewon99 <tlswodnjs99@naver.com>
2025-05-06 19:22:35 +0900

    feat: 영화 할인 정책에 대한 기능 구현

---
+++ src/main/java/_02_movie/Movie.java
@@ -0,0 +1,23 @@
+package _02_movie;
+
+public class Movie {
+    private String title;
+    private Duration runningTime;
+    private Money fee;
+    private DiscountPolicy discountPolicy;
+
+    public Movie(final String title, final Duration runningTime, final Money fee, final DiscountPolicy discountPolicy) {
+        this.title = title;
+        this.runningTime = runningTime;
+        this.fee = fee;
+        this.discountPolicy = discountPolicy;
+    }
+
+    public Money getFee() {
+        return fee;
+    }
+
+    public Money calculateMovieFee(Screening screening) {
+        return fee.minus(discountPolicy.calculateDiscountAmount(screening));
+    }
+}
