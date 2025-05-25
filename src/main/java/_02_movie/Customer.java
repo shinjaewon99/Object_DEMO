41de75ac066ac7f40c399eb02f79ee0fa8604f8b
shinjaewon99 <tlswodnjs99@naver.com>
2025-05-25 09:36:12 +0900

    feat: 고객 클래스

---
+++ src/main/java/_02_movie/Customer.java
@@ -0,0 +1,11 @@
+package _02_movie;
+
+public class Customer {
+    private String name;
+    private String id;
+
+    public Customer(final String name, final String id) {
+        this.name = name;
+        this.id = id;
+    }
+}
