8caa64575a1bb70552ebaacb285dd744959c583d
shinjaewon99 <tlswodnjs99@naver.com>
2025-05-25 09:36:34 +0900

    feat: 영화 예매를 위한 데이터 클래스

---
+++ src/main/java/_04_movie_data_system/Customer.java
@@ -0,0 +1,11 @@
+package _04_movie_data_system;
+
+public class Customer {
+    private String name;
+    private String id;
+
+    public Customer(final String name, final String id) {
+        this.name = name;
+        this.id = id;
+    }
+}
---
+++ src/main/java/_04_movie_data_system/Screening.java
@@ -0,0 +1,33 @@
+package _04_movie_data_system;
+
+import java.time.LocalDateTime;
+
+public class Screening {
+    private Movie movie;
+    private int sequence;
+    private LocalDateTime whenScreened;
+
+    public Movie getMovie() {
+        return movie;
+    }
+
+    public void setMovie(final Movie movie) {
+        this.movie = movie;
+    }
+
+    public int getSequence() {
+        return sequence;
+    }
+
+    public void setSequence(final int sequence) {
+        this.sequence = sequence;
+    }
+
+    public LocalDateTime getWhenScreened() {
+        return whenScreened;
+    }
+
+    public void setWhenScreened(final LocalDateTime whenScreened) {
+        this.whenScreened = whenScreened;
+    }
+}
