1ac0b04338432cc9d70fdcce680687da54a3a57d
shinjaewon99 <tlswodnjs99@naver.com>
2025-06-01 14:39:27 +0900

    feat : 사용하지 않는 의존성 제거

--- src/main/java/_04_movie_data_system/Screening.java
+++ src/main/java/_04_movie_data_system/Screening.java
@@ -7,27 +7,28 @@ public class Screening {
     private int sequence;
     private LocalDateTime whenScreened;
 
-    public Movie getMovie() {
-        return movie;
-    }
-
-    public void setMovie(final Movie movie) {
+    public Screening(final Movie movie, final int sequence, final LocalDateTime whenScreened) {
         this.movie = movie;
-    }
-
-    public int getSequence() {
-        return sequence;
-    }
-
-    public void setSequence(final int sequence) {
         this.sequence = sequence;
-    }
-
-    public LocalDateTime getWhenScreened() {
-        return whenScreened;
-    }
-
-    public void setWhenScreened(final LocalDateTime whenScreened) {
         this.whenScreened = whenScreened;
+    }
+
+    public Money calculateFee(int audienceCount) {
+        switch (movie.getMovieType()) {
+            case AMOUNT_DISCOUNT:
+                if (movie.isDiscountable(whenScreened, sequence)) {
+                    return movie.calculateAmountDiscountedFee().times(audienceCount);
+                }
+                break;
+            case PERCENT_DISCOUNT:
+                if (movie.isDiscountable(whenScreened, sequence)) {
+                    return movie.calculatePercentDiscountedFee().times(audienceCount);
+                }
+                break;
+            case NONE_DISCOUNT:
+                return movie.calculatedNoneDiscountedFee().times(audienceCount);
+        }
+
+        return movie.calculatedNoneDiscountedFee().times(audienceCount);
     }
 }
