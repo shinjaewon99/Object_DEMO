79d1b145262d9bee4501f9fd4bfca36c2c894300
shinjaewon99 <tlswodnjs99@naver.com>
2025-06-01 14:24:38 +0900

    feat : 할인 정책에 따른 메소드 추가

--- src/main/java/_04_movie_data_system/DiscountCondition.java
+++ src/main/java/_04_movie_data_system/DiscountCondition.java
@@ -16,6 +16,23 @@ public class DiscountCondition {
         return type;
     }
 
+    public boolean isDiscountable(DayOfWeek dayOfWeek, LocalTime time) {
+        if (type != DiscountConditionType.PERIOD) {
+            throw new IllegalArgumentException();
+        }
+
+        return this.dayOfWeek.equals(dayOfWeek) && this.startTime.compareTo(time) <= 0
+                && this.endTime.compareTo(time) >= 0;
+    }
+
+    public boolean isDiscountable(int sequence) {
+        if (type != DiscountConditionType.SEQUENCE) {
+            throw new IllegalArgumentException();
+        }
+        return this.sequence == sequence;
+    }
+
+
     public void setType(final DiscountConditionType type) {
         this.type = type;
     }
@@ -51,4 +68,5 @@ public class DiscountCondition {
     public void setEndTime(final LocalTime endTime) {
         this.endTime = endTime;
     }
+
 }
--- src/main/java/_04_movie_data_system/Movie.java
+++ src/main/java/_04_movie_data_system/Movie.java
@@ -1,6 +1,7 @@
 package _04_movie_data_system;
 
 import java.time.Duration;
+import java.time.LocalDateTime;
 import java.util.List;
 
 public class Movie {
@@ -51,5 +52,46 @@ public class Movie {
 
     public void setDiscountPercent(final double discountPercent) {
         this.discountPercent = discountPercent;
+    }
+
+    public boolean isDiscountable(LocalDateTime whenScreened, int sequence) {
+        for (DiscountCondition condition : discountConditions) {
+            // 할인 조건이 기간 조건이라면
+            if (condition.getType() == DiscountConditionType.PERIOD) {
+                if (condition.isDiscountable(whenScreened.getDayOfWeek(), whenScreened.toLocalTime())) {
+                    return true;
+                } else {
+                    // 할인 조건이 순번 조건이라면
+                    if (condition.isDiscountable(sequence)) {
+                        return true;
+                    }
+                }
+            }
+        }
+        return false;
+    }
+
+    public Money calculateAmountDiscountedFee() {
+        if (movieType != MovieType.AMOUNT_DISCOUNT) {
+            throw new IllegalArgumentException();
+        }
+
+        return fee.minus(discountAmount);
+    }
+
+    public Money calculatePercentDiscountedFee() {
+        if (movieType != MovieType.PERCENT_DISCOUNT) {
+            throw new IllegalArgumentException();
+        }
+
+        return fee.minus(fee.times(discountPercent));
+    }
+
+    public Money calculatedNoneDiscountedFee() {
+        if (movieType != MovieType.NONE_DISCOUNT) {
+            throw new IllegalArgumentException();
+        }
+
+        return fee;
     }
 }
