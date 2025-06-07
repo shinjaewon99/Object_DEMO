71a91b53dd28dd773d3c6ea734eaf720bbf617ca
shinjaewon99 <tlswodnjs99@naver.com>
2025-06-07 12:58:27 +0900

    feat : 상영시간 / 상영순번에 따른 할인 메소드 추가

--- src/main/java/_05_movie_responsibility/DiscountCondition.java
+++ src/main/java/_05_movie_responsibility/DiscountCondition.java
@@ -1,16 +1,34 @@
 package _05_movie_responsibility;
 
+import _04_movie_data_system.DiscountConditionType;
+
 import java.time.DayOfWeek;
 import java.time.LocalDateTime;
 
 public class DiscountCondition {
-    private DiscountCondition type;
+    private DiscountConditionType type;
     private int sequence;
     private DayOfWeek dayOfWeek;
     private LocalDateTime startTime;
     private LocalDateTime endTime;
 
     public boolean isSatisfiedBy(Screening screening) {
+        if (type == DiscountConditionType.PERIOD) {
+            return isSatisfiedByPeriod(screening);
+        }
 
+        return isSatisfiedBySequence(screening);
     }
+
+    private boolean isSatisfiedByPeriod(Screening screening) {
+        return dayOfWeek.equals(screening.getWhenScreened().getDayOfWeek()) &&
+                startTime.compareTo(screening.getWhenScreened().toLocalTime()) <= 0 &&
+                endTime.isAfter(screening.getWhenScreened().toLocalTime()) >= 0;
+    }
+
+    private boolean isSatisfiedBySequence(Screening screening) {
+        return sequence == screening.getSequence();
+    }
+
+
 }
