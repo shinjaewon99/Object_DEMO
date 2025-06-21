ea7361c57855d3197da9eb8406724062e6b1bb76
shinjaewon99 <tlswodnjs99@naver.com>
2025-06-21 14:00:46 +0900

    feat : Money 객체 비교를 위한 equals / hashcode 오버라이딩

--- src/main/java/_02_movie/Money.java
+++ src/main/java/_02_movie/Money.java
@@ -34,4 +34,17 @@ public class Money {
     public boolean isGreaterTahOrEqual(Money other) {
         return amount.compareTo(other.amount) >= 0;
     }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Money)) return false;
+        Money other = (Money) o;
+        return this.amount.equals(other.amount);
+    }
+
+    @Override
+    public int hashCode() {
+        return amount.hashCode();
+    }
 }
--- src/test/java/movie_test.java
+++ src/test/java/movie_test.java
@@ -1,17 +1,16 @@
 import _02_movie.*;
-import org.junit.jupiter.api.Assertions;
+import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 import java.time.*;
 import java.util.List;
-import java.util.stream.Collectors;
 
 @DisplayName("Movie 테스트")
 public class movie_test {
     private final Money FIX_MONEY = Money.wons(100);
     private final double FIX_DISCOUNT_PERCENT = 0.1;
-    private final LocalDateTime 월요일 = LocalDate.of(2025, Month.JUNE, 21).atStartOfDay();
+    private final LocalDateTime 월요일 = LocalDate.of(2025, Month.JUNE, 23).atStartOfDay();
     private final LocalDateTime 화요일 = 월요일.plusDays(1);
     private final LocalDateTime 수요일 = 월요일.plusDays(2);
     private final LocalDateTime 목요일 = 월요일.plusDays(3);
@@ -34,4 +33,41 @@ public class movie_test {
             new PeriodCondition(DayOfWeek.MONDAY, LocalTime.of(10, 0), LocalTime.of(11, 59)),
             new PeriodCondition(DayOfWeek.FRIDAY, LocalTime.of(10, 0), LocalTime.of(20, 59))
     );
+
+    class FIX_MOVIE_범죄도시 {
+        Movie 범죄도시_생성() {
+            return settingMovie_범죄도시();
+        }
+    }
+
+    @Test
+    @DisplayName("범죄도시_고정할인")
+    void 범죄도시_고정할인() {
+        FIX_MOVIE_범죄도시 범죄도시_생성 = new FIX_MOVIE_범죄도시();
+        Movie 범죄도시 = 범죄도시_생성.범죄도시_생성();
+
+        final List<LocalDateTime> 할인_조건에_맞는_상영_시작_시간들 = List.of(
+                // 월요일 시간 boundary 설정
+                월요일.withHour(10).withMinute(0),
+                월요일.withHour(11).withMinute(59),
+                월요일.withHour(10).withMinute(1),
+                월요일.withHour(11).withMinute(58),
+
+                // 목요일 시간 boundary 설정
+                목요일.withHour(10).withMinute(0),
+                목요일.withHour(20).withMinute(59),
+                목요일.withHour(10).withMinute(1),
+                목요일.withHour(11).withMinute(58)
+        );
+
+        List<Screening> screenings = 할인_조건에_맞는_상영_시작_시간들.stream()
+                .map(상영시간 -> new Screening(범죄도시, 1, 상영시간)).toList();
+
+        Money 기본요금 = 범죄도시.getFee();
+
+        for (Screening screening : screenings) {
+            Money 계산된금액 = 범죄도시.calculateMovieFee(screening);
+            Assertions.assertThat(계산된금액).isEqualTo(기본요금.minus(FIX_MONEY));
+        }
+    }
 }
